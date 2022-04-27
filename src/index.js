fb753874affd2ed4b9ed484f95dffea3d1fb2507
jsw <allmie@naver.com>
2022-04-27 10:51:45 +0900

    Express, Middleware

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-const a = (b) => console.log(b);
+import express from 'express';
+import morgan from 'morgan';
 
-console.log(a('zdddd'));
+const app = express();
+
+const handleHome = (req, res) => {
+  return res.send('in root');
+};
+
+app.use(morgan('tiny'));
+app.get('/', handleHome);
+
+const handleListening = () => console.log('Server listening on port 4000');
+
+app.listen(4000, handleListening);
