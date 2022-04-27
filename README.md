38c8bec789761bdd990fc6b0d79a1e4a44343cef
jsw <allmie@naver.com>
2022-04-27 13:58:25 +0900

    Router

---
+++ README.md
@@ -0,0 +1,23 @@
+# Wetube
+
+## Router
+
+### Global
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+### User
+
+/users/edit -> Edit Profile
+/users/delete -> Delete Profile
+
+### Video
+
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/videos/comment -> Comment on a Video
+/videos/comment/delete -> Delete a Comment on a Video
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import morgan from 'morgan';
+import globalRouter from './routers/globalRouter';
+import userRouter from './routers/userRouter';
+import videoRouter from './routers/videoRouter';
 
 const app = express();
-
-const handleHome = (req, res) => {
-  return res.send('in root');
-};
-
 app.use(morgan('tiny'));
-app.get('/', handleHome);
+
+app.use('/', globalRouter);
+app.use('/users', userRouter);
+app.use('/videos', videoRouter);
 
 const handleListening = () => console.log('Server listening on port 4000');
 
