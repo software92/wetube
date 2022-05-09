ac6daa5dc96237c7bb6546af0ae4e10b9d1903ae
jsw <allmie@naver.com>
2022-05-09 14:22:47 +0900

    Pug literation

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,23 @@
-export const trending = (req, res) => res.render('home', { pageTitle: 'Home' });
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: 'a',
+      description: 'aa',
+      createdAt: 122,
+    },
+    {
+      title: 'b',
+      description: 'bbbb',
+      createdAt: 324,
+    },
+    {
+      title: 'c',
+      description: 'ccc',
+      createdAt: 415,
+    },
+  ];
+  return res.render('home', { pageTitle: 'Home', videos });
+};
 export const search = (req, res) => res.send('Search');
 export const upload = (req, res) => res.send('Upload Video');
 export const see = (req, res) => {
